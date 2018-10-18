fa443d1f0a36c2ad55ccbe6c5687e07d213a8556
Michael Soares <mikey.soares@gmail.com>
2018-10-18 15:43:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function destructivelyRemoveLastKitten() {
 }
 var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyRemoveFirstKitten() {
-  kettens.shift('Milo')
+  kettens.shift()
   return kittens
 }
